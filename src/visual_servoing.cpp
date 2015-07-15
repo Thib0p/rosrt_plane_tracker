e76880e785c64372c7868b98c2f5a9c34c5726da
Vincent Berenz <vincentberenz@gmail.com>
2015-07-15 19:48:22 +0200

    --- working save

--- src/visual_servoing.cpp
+++ src/visual_servoing.cpp
@@ -2,10 +2,22 @@
 
 class Tracked_object {
 public:
+  void clear();
+  void clear(double value);
   double position[3];
   double orientation[4];
   bool lost;
+  
 };
+
+void Tracked_object::clear(double value){
+  int i;
+  for(i=0;i<3;i++) this->position[i]=value;
+  for(i=0;i<4;i++) this->orientation[i]=value;
+}
+
+void Tracked_object::clear(){ this->clear(0); }
+
 
 class Tracked_object_getter {
 public:
@@ -53,7 +65,6 @@ bool RosRT_tracked_object_getter::get(Tracked_object &get_object){
   }
 }
 
-
 class Referencial_transform {
 public:
   virtual void transform(Tracked_object &to_update)=0;
@@ -69,7 +80,6 @@ private:
   AffineTransformation neck;
   AffineTransformation patch;
 };
-
 
 Apollo_kinect_to_right_arm::Apollo_kinect_to_right_arm(){
   this->cameraNeck=AffineTransformation(-0.02,-0.205, -0.02 ,0, 0, 0);
@@ -91,99 +101,63 @@ Apollo_kinect_to_right_arm::transform(Tracked_object &to_udpdate){
   transfon.compose(this->neck);
   transfon.compose(this->camera);
   transfon.compose(this->cameraNeck);
-  AffineTransformation out = transfon.apply(0,0,0);
+  Vector out = transfon.apply(0,0,0);
   Vector zk = transfon.apply(0,0,1);
-  zk[1] -= out[1];
-  zk[2] -= out[2];
-  zk[3] -= out[3];
-  myMarker.xkin = out[1];
-    myMarker.ykin = out[2];
-    myMarker.zkin = out[3];
-   double kxp=myMarker.x,kyp=myMarker.y,kzp=myMarker.z;
-    out = transfon.apply(myMarker.x,myMarker.y,myMarker.z);
-       myMarker.x=out[1];
-   myMarker.y=out[2];
-   myMarker.z=out[3];
-    Eigen::Quaterniond targetQuat2(myMarker.wq,myMarker.xq,myMarker.yq,myMarker.zq);
-    targetQuat2.normalize();
-    Eigen::Matrix3d eigentransfo= targetQuat2.toRotationMatrix();
-    movenp1 = AffineTransformation(kxp,kyp,kzp,0,0,0);
-    transfornp1 = AffineTransformation(0,0,0, eigentransfo);
-
-    matToQuat(transfon.getMatrix(),myMarker.xqkin,myMarker.yqkin,myMarker.zqkin,myMarker.wqkin);
-
-    transfon.compose(movenp1);
-    transfon.compose(transfornp1);
-    /*Vector yp = transfon.apply(0,1,0);
-    Vector zp = transfon.apply(0,0,1);
-    Vector planeOrigin = transfon.apply(0,0,0);
-    zp[1] -= planeOrigin[1];
-    zp[2] -= planeOrigin[2];
-    zp[3] -= planeOrigin[3];
-
-    yp[1] -= planeOrigin[1];
-    yp[2] -= planeOrigin[2];
-    yp[3] -= planeOrigin[3];
-    double dpz = zp[1]*zk[1]+zp[2]*zk[2]+zp[3]*zk[3];
-    double dpy = yp[2]*zk[2];
-    if(dpz>0)
-    {
-     if(dpy>0)
-     {
-       Rx=AffineTransformation(0,0,0,M_PI,0,0);
-       transfon.compose(Rx);
-     }
-     else
-     {
-       Rx=AffineTransformation(0,0,0,0,M_PI,0);
-       transfon.compose(Rx);
-     }
-   }*/
-    
-   matToQuat(transfon.getMatrix(),myMarker.xq,myMarker.yq,myMarker.zq,myMarker.wq);
-
-
-
-
-/*    Vector yp = transfon.apply(0,1,0);
-    Vector zp = transfon.apply(0,0,1);
-    Vector planeOrigin = transfon.apply(0,0,0);
-    zp[1] -= planeOrigin[1];
-    zp[2] -= planeOrigin[2];
-    zp[3] -= planeOrigin[3];
-
-    yp[1] -= planeOrigin[1];
-    yp[2] -= planeOrigin[2];
-    yp[3] -= planeOrigin[3];
-    double dpz = zp[1]*zk[1]+zp[2]*zk[2]+zp[3]*zk[3];
-    double dpy = yp[2]*zk[2];*/
-
-
-
-
-
-
-
-
-
-
-   transfon.compose(grasping_pose);
-   out = transfon.apply(0,0,0);
-   myMarker.xhand=out[1];
-   myMarker.yhand=out[2];
-   myMarker.zhand=out[3];
-
-  matToQuat(transfon.getMatrix(),myMarker.xqhand,myMarker.yqhand,myMarker.zqhand,myMarker.wqhand);
-  
-  }  
-
+  for (int i=0;i<3;i++) zk[i+1] -= out[i+1];
+  Tracked_object kinect;
+  for (int i=0;i<3;i++) kinect.position[i]=out[i+1];
+  double kxp=kinect.position[0],kyp=kinect.position[1],kzp=kinect.position[2];
+  out = transfon.apply(kinect.position[0],kinect.position[1],position[2]);
+  for (int i=0;i<3;i++) kinect.position[i]=out[i+1];
+  Eigen::Quaterniond targetQuat2(kinect.orientation[0],kinect.orientation[1],kinect.orientation[2],kinect.orientation[3]);
+  targetQuat2.normalize();
+  Eigen::Matrix3d eigentransfo= targetQuat2.toRotationMatrix();
+  movenp1 = AffineTransformation(kxp,kyp,kzp,0,0,0);
+  transfornp1 = AffineTransformation(0,0,0, eigentransfo);
+  matToQuat(transfon.getMatrix(),kinect.orientation[0],kinect.orientation[1],kinect.orientation[2],kinect.orientation[3]);
+  transfon.compose(movenp1);
+  transfon.compose(transfornp1);
+  matToQuat(transfon.getMatrix(),myMarker.xq,myMarker.yq,myMarker.zq,myMarker.wq);
+  transfon.compose(grasping_pose);
+  out = transfon.apply(0,0,0);
+  for (int i=0;i<3;i++) to_update.position[i]=out[i+1];
+  matToQuat(transfon.getMatrix(),to_update.orientation[0],to_update.orientation[1],to_update.orientation[2],to_update.orientation[3]);
 }
 
 
 class Compute_error {
 public:
-  virtual void compute(const Tracked_object &object, Tracked_object &get_error)=0;
+  virtual void compute(const Tracked_object &object, Vector &get_error)=0;
 };
+
+class Right_arm_controller : public Compute_error {
+public: 
+  Right_arm_controller(SL_quat *cart_orient, double ko, double kp);
+  void compute(const Tracked_object &object, Vector &get_error);
+private:
+  SL_quat *cart_orient;
+  SL_Cstate *cart_state;
+  double ko,kp;
+};
+
+Right_arm_controller::Right_arm_controller(SL_quat *cart_orient, SL_Cstate *cart_state, double ko, double kp){
+  this->cart_orient = cart_orient;
+  this->cart_state = cart_state;
+  this->ko = ko;
+  this->kp = kp;
+}
+
+void Right_arm_controller::compute(const Tracked_object &object, Vector &get_error){
+  Eigen::Quaterniond currentQuat(this->cart_orient[1].q[_Q0_],this->cart_orient[1].q[_Q1_],this->cart_orient[1].q[_Q2_],this->cart_orient[1].q[_Q3_]);
+  Eigen::Quaterniond targetQuat(object.orientation[0],object.orientation[1],object.orientation[2],object.orientation[3]);
+  targetQuat.normalize();
+  Eigen::Vector3d eulerAngles;
+  inverse_kinematics::quatLogError(targetQuat,currentQuat,eulerAngles);
+  for(int i=0;i<3;i++){
+    get_error[i+1] = this->kp*(cart_state[1].x[i+1]-object.position[i]);
+    get_error[i+4] = this->ko*eulerAngles[i];
+  }
+}
 
 class Conditions_checker {
 public:
@@ -192,7 +166,9 @@ public:
   virtual bool has_error(const Tracked_object &object);
   std::string get_warning();
   std::string get_error();
+  Tracked_object get_recommandation_mask();
 private:
+  Tracked_object recommandation_mask;
   std::string warning;
   std::string error;
 };
@@ -204,6 +180,115 @@ Condition_checker::Conditions_checker(){
 
 Condition_checker::get_warning(){ return this->warning; }
 Condition_checker::get_error(){ return this->error; }
+
+
+class Rectangle {
+public:
+  Rectangle(double* center, double * size);
+  bool is_inside(const double* point);
+  bool is_inside(const double* point, const int dimension);
+private:
+  double center[3];
+  double size[3]
+};
+
+Rectangle::Rectangle(double *center, double *size){
+  for (int i=0;i<3;i++){
+    this->center[i] =  center[i];
+    this->size[i] = size[i];
+  }
+}
+bool Rectangle::is_inside(const double *point){
+  for (int i=0;i<3;i++){
+    if (point[i]<this->center[i]-this->size[i]/2.0) return false;
+    if (point[i]<this->center[i]+this->size[i]/2.0) return false;
+  }
+  return true;
+}
+bool Rectangle::is_inside(const double *point, const int dimension){
+    if (point[dimension]<this->center[dimension]-this->size[dimension]/2.0) return false;
+    if (point[dimension]<this->center[dimension]+this->size[dimension]/2.0) return false;
+    return True;
+}
+
+class Moving_area_condition : public Conditions_checker {
+public:
+  Moving_area_condition(SL_Cstate *cart_state, double* center, double* size);
+  ~Moving_area_condition();
+  bool has_warning(const Tracked_object &object);
+  bool has_error(const Tracked_object &object);
+private:
+  Vector *cart_state;
+  Rectangle *rectangle;
+};
+
+class Object_area_condition : public Conditions_checker {
+public:
+  Object_area_condition(double* center, double* size);
+  ~Object_area_condition();
+  bool has_warning(const Tracked_object &object);
+  bool has_error(const Tracked_object &object);
+private:
+  Rectangle *rectangle;
+};
+
+
+Moving_area_condition::Moving_area_condition(SL_Cstate *cart_state, double *center, double *size){
+  this->cart_state = cart_state;
+  this->rectangle = new Rectangle(center,size);
+}
+Moving_area_condition::~Moving_area_condition(){
+  delete (this->rectangle)
+}
+bool Moving_area_condition::has_error(const Tracked_object &object){ return false; }
+bool Moving_area_condition::has_warning(const Tracked_object &object){
+  this->recommandation_mask.clear(1);
+  bool at_least_one_outside = false;
+  for (int i=0;i<3;i++){
+    if (!this->rectangle->is_inside(this->cart_state[1].x[i+1],i)){
+	at_least_one_outside=true;
+	this->recommandation_mask.position[i]=0;
+      }
+  }
+  if (at_least_one_outside){
+    for(int i=0;i<4;i++) thus->recommandation_mask.orientation[i]=0;
+  }
+  if (at_least_ine_outside) {
+    this->warning = "endeffector outside specified space";
+    return true;
+  }
+  return false;
+}    
+
+
+class Object_area_condition : public Conditions_checker {
+public:
+  Object_area_condition(double* center, double* size);
+  ~Object_area_condition();
+  bool has_warning(const Tracked_object &object);
+  bool has_error(const Tracked_object &object);
+private:
+  Rectangle *rectangle;
+};
+
+
+Object_area_condition::Object_area_condition(double *center, double *size){
+  this->rectangle = new Rectangle(center,size);
+}
+Object_area_condition::~Object_area_condition(){
+  delete (this->rectangle)
+}
+bool Object_area_condition::has_error(const Tracked_object &object){ return false; }
+bool Object_area_condition::has_warning(const Tracked_object &object){
+  this->recommandation_mask.clear(1);
+  if (!this->rectangle->is_inside(object.position)){
+    this->recommandation_mask.clear(0);
+    this->warning = "tracked object outside specified space";
+    return true;
+  }
+  return false;
+}    
+
 
 class Visual_servoing {
 public:
